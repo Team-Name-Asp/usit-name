3c4527ce75ed46db12044ede617f1f5c0d4e3307
martinst1 <martinst@gmail.com>
2017-02-15 20:39:59 +0200

    Added job service tests

---
+++ TelerikColours.Tests/Services/JobServiceTests/ConstructorShould.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using Moq;
+using NUnit.Framework;
+using Repositories.Contracts;
+using TelerikColours.Services;
+
+namespace TelerikColours.Tests.Services.JobServiceTests
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void Throw_WithExpectedMessage_When_IRepositoryOfJob_IsNull()
+        {
+            // Arrange
+            string expectedContainingString = "JobRepository";
+
+            // Act and Assert
+            var output = Assert.Throws<NullReferenceException>(() => new JobService(null));
+
+            StringAssert.Contains(expectedContainingString, output.Message);
+        }
+    }
+}
---
+++ TelerikColours.Tests/Services/JobServiceTests/GetAllJobsShould.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using Moq;
+using NUnit.Framework;
+using Repositories.Contracts;
+using TelerikColours.Services;
+
+namespace TelerikColours.Tests.Services.JobServiceTests
+{
+    [TestFixture]
+    public class GetAllJobsShould
+    {
+        [Test]
+        public void Call_IRepositoryOfJobs_Method_Method_GetAll_Once()
+        {
+            // Arrange
+            var jobRepositoryStub = new Mock<IRepository<Job>>();
+            var jobService = new JobService(jobRepositoryStub.Object);
+
+            // Act
+            jobService.GetAllJobs();
+
+            // Assert
+            jobRepositoryStub.Verify(j => j.GetAll(), Times.Once());
+        }
+    }
+}
