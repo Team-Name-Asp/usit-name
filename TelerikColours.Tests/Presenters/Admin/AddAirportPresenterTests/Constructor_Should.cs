6833017afd3d4732bec80a59d94d542ff1bacef0
BoykoBonev <boykobonev@outlook.com>
2017-02-18 17:34:50 +0200

    Added tests for AddAirportPresenter

---
+++ TelerikColours.Tests/Presenters/Admin/AddAirportPresenterTests/Constructor_Should.cs
@@ -0,0 +1,57 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+using TelerikColours.Mvp.Admin.AddAirport;
+using TelerikColours.Services.Contracts;
+
+namespace TelerikColours.Tests.Presenters.Admin.AddAirportPresenterTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ThrowNullReferenceExceptionWithMessageContaining_ILocationService_WhenLicationServiceIsNull()
+        {
+            // Arrange
+            var factoryServiceStub = new Mock<IFactoryService>();
+            var viewStub = new Mock<IAddAirportView>();
+
+            // Act and Assert
+            Assert.That(
+              () => new AddAirportPresenter(viewStub.Object, null, factoryServiceStub.Object),
+              Throws.InstanceOf<NullReferenceException>().With.Message.Contains("ILocationService"));
+        }
+
+        [Test]
+        public void ThrowNullReferenceExceptionWithMessageContaining_IFactoryService_WhenIFactoryServiceIsNull()
+        {
+            // Arrange
+            var viewStub = new Mock<IAddAirportView>();
+            var locationServiceStub = new Mock<ILocationService>();
+
+            // Act and Assert
+            Assert.That(
+              () => new AddAirportPresenter(viewStub.Object, locationServiceStub.Object, null),
+              Throws.InstanceOf<NullReferenceException>().With.Message.Contains("IFactoryService"));
+        }
+
+        [Test]
+        public void NotThrow_WhenEveythingIsSet()
+        {
+            // Arrange
+            var viewStub = new Mock<IAddAirportView>();
+            var factoryServiceStub = new Mock<IFactoryService>();
+            var locationServiceStub = new Mock<ILocationService>();
+
+            // Act and Assert
+            Assert.DoesNotThrow(() => new AddAirportPresenter(viewStub.Object, locationServiceStub.Object, factoryServiceStub.Object));
+        }
+
+    }
+}
+
---
+++ TelerikColours.Tests/Presenters/Admin/AddAirportPresenterTests/View_InitCountries_Should.cs
@@ -0,0 +1,40 @@
+﻿using Models;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using TelerikColours.Mvp.Admin.AddAirport;
+using TelerikColours.Services.Contracts;
+
+namespace TelerikColours.Tests.Presenters.Admin.AddAirportPresenterTests
+{
+    [TestFixture]
+    public class View_InitCountries_Should
+    {
+        [Test]
+        public void AddCoutriesToViewModel_WhenInitCountriesEventIsRaised()
+        {
+            // Arrange
+            var locationServiceMock = new Mock<ILocationService>();
+            var factoryServiceStub = new Mock<IFactoryService>();
+            var viewMock = new Mock<IAddAirportView>();
+
+            var addAirportPresenter = new AddAirportPresenter(viewMock.Object, locationServiceMock.Object, factoryServiceStub.Object);
+            viewMock.Setup(v => v.Model).Returns(new AddAirportViewModel());
+
+            var expectedCountries = new List<Country>()
+            {
+                new Country("FirstCOuntry"),
+                new Country("SecondCountry")
+            };
+
+            locationServiceMock.Setup(l => l.GetAllCountries()).Returns(expectedCountries);
+
+            // Act
+            viewMock.Raise(v => v.InitCountries += null, EventArgs.Empty);
+
+            // Assert
+            CollectionAssert.AreEquivalent(expectedCountries, viewMock.Object.Model.Countries);
+        }
+    }
+}
