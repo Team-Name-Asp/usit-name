fe365db9c02d2f61e22f4061f50181e71018e665
BoykoBonev <boykobonev@outlook.com>
2017-02-22 01:16:10 +0200

    small fixes

---
+++ TelerikColours.Tests/Services/UserServiceTests/GetJobHistory_Should.cs
@@ -0,0 +1,77 @@
+﻿using Models;
+using Moq;
+using NUnit.Framework;
+using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TelerikColours.Services;
+using TelerikColours.Services.Contracts.Factories;
+
+namespace TelerikColours.Tests.Services.UserServiceTests
+{
+    public class GetJobHistory_Should
+    {
+        [Test]
+        public void ReturnExpectedCollectionOfUserWithPassedId_WhenPerformedExpectedExpressions()
+        {
+            // Arrange
+            var flightRepositoryStub = new Mock<IRepository<Flight>>();
+            var userRepositoryStub = new Mock<IRepository<ApplicationUser>>();
+            var unitOfWorkStub = new Mock<IUnitOfWork>();
+            var airportFactoryStub = new Mock<IAirportFactory>();
+            var ticketRepositoryStub = new Mock<IRepository<Ticket>>();
+            var userService = new UserService(userRepositoryStub.Object, flightRepositoryStub.Object, unitOfWorkStub.Object, airportFactoryStub.Object, ticketRepositoryStub.Object);
+
+
+            var firstDateOfDeparture = new DateTime(2016, 10, 10, 10, 5, 5, 1);
+            var secondDateOfDeparture = new DateTime(2016, 10, 10, 11, 5, 5, 3);
+            var thirdDateOfDeparture = new DateTime(2016, 10, 10, 12, 5, 5, 3);
+            string userId = "GuidGuid";
+
+            var jobs = new List<Job>()
+            {
+                new Job()
+                {
+
+                        StartDate = secondDateOfDeparture,
+                    Id = It.IsAny<int>()
+                },
+                new Job()
+                {
+
+                        StartDate = thirdDateOfDeparture,
+                    Id = It.IsAny<int>()
+                },
+                 new Job()
+                {
+
+                    StartDate = firstDateOfDeparture,
+                    Id = It.IsAny<int>()
+                }
+            };
+
+            var user = new ApplicationUser()
+            {
+                Id = userId,
+                Jobs = jobs
+            };
+
+            var userCollection = new List<ApplicationUser>();
+            userCollection.Add(user);
+
+            var userCollectionAsQuaryable = userCollection.AsQueryable();
+
+            userRepositoryStub.Setup(u => u.All).Returns(userCollectionAsQuaryable);
+
+
+            var expectedCollection = userCollectionAsQuaryable.Where(u => u.Id == userId).SelectMany(p => p.Jobs).OrderBy(t => t.StartDate);
+
+            // Act
+            var actualCollection = userService.GetJobsHistory(userId);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedCollection.ToList(), actualCollection.ToList());
+        }
+    }
+}
