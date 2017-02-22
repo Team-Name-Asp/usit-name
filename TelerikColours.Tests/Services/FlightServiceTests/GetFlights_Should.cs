d0a85c9f5f3196135d32408a14fab98cfae82020
BoykoBonev <boykobonev@outlook.com>
2017-02-22 12:19:54 +0200

    Added tests for UnitOfWork

--- TelerikColours.Tests/Services/FlightServiceTests/GetFlights_Should.cs
+++ TelerikColours.Tests/Services/FlightServiceTests/GetFlights_Should.cs
@@ -5,6 +5,7 @@
 //using System;
 //using System.Collections;
 //using System.Collections.Generic;
+//using System.Linq;
 //using TelerikColours.Services;
 //using TelerikColours.Services.Contracts.Factories;
 
@@ -13,20 +14,43 @@
 //    [TestFixture]
 //    public class GetFlights_Should
 //    {
+//        //[Test]
+//        //public void CallGetAll_OnAirportRepositoryOnce()
+//        //{
+//        //    // Arrange
+//        //    var flightRepositoryStub = new Mock<IRepository<Flight>>();
+//        //    var airportRepositoryMock = new Mock<IRepository<Airport>>();
+//        //    var cityRepositoryStub = new Mock<IRepository<City>>();
+//        //    var mappedFactory = new Mock<IMappedClassFactory>();
+//        //    var unitOfWorkStub = new Mock<IUnitOfWork>();
+//        //    var flightService = new FlightService(airportRepositoryMock.Object, unitOfWorkStub.Object, flightRepositoryStub.Object, cityRepositoryStub.Object, mappedFactory.Object);
+
+//        //    // Act
+//        //    flightService.GetFlights(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<DateTime>().AddDays(10), It.IsAny<int>());
+
+//        //    // Assert
+//        //    airportRepositoryMock.Verify(a => a.GetAll(), Times.Once);
+//        //}
+
 //        [Test]
-//        public void Set()
+//        public void CallCreateFlightNodeOnFactoryServiceAsManyTimesAsAirportCollection()
 //        {
 //            // Arrange
 //            var flightRepositoryStub = new Mock<IRepository<Flight>>();
-//            var airportRepositoryStub = new Mock<IRepository<Airport>>();
+//            var airportRepositoryMock = new Mock<IRepository<Airport>>();
 //            var cityRepositoryStub = new Mock<IRepository<City>>();
 //            var mappedFactory = new Mock<IMappedClassFactory>();
 //            var unitOfWorkStub = new Mock<IUnitOfWork>();
-//            var flightService = new FlightService(airportRepositoryStub.Object, unitOfWorkStub.Object, flightRepositoryStub.Object, cityRepositoryStub.Object, mappedFactory.Object);
+//            var flightService = new FlightService(airportRepositoryMock.Object, unitOfWorkStub.Object, flightRepositoryStub.Object, cityRepositoryStub.Object, mappedFactory.Object);
 
-//            var travenDate = new DateTime(2017, 10, 10);
-//            // Airport setup
-//            airportRepositoryStub.Setup(a => a.GetAll()).Returns()
+//            var airports = this.GetAirports().ToList();
+//            airportRepositoryMock.Setup(a => a.GetAll()).Returns(airports);
+
+//            // Act
+//            flightService.GetFlights(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<DateTime>().AddDays(10), It.IsAny<int>());
+
+//            // Assert
+//            mappedFactory.Verify(f => f.CreateSecondFlightNode(It.IsAny<int>()), Times.Exactly(airports.Count));
 //        }
 
 //        private IEnumerable<Airport> GetAirports()
@@ -39,7 +63,9 @@
 //            var dublinAirport = new Airport() { Id = 5, Name = "Dublin" };
 //            var varnaAirport = new Airport() { Id = 6, Name = "Varna" };
 
+//            var airports = new List<Airport>() { sofiaAirport, berlinAirport, erfurtAirport, burgasAirport, dublinAirport, varnaAirport };
 
+//            return airports;
 //        }
 //    }
 //}
