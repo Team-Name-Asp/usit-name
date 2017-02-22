22eafbfe20b9134cb6a552bcb70cfcc46c95b1fc
martinst1 <martinst@gmail.com>
2017-02-22 03:35:26 +0200

    more job service tests

---
+++ TelerikColours.Tests/Services/JobServiceTests/GetAllJobsFromByTerm_Should.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using Moq;
+using NUnit.Framework;
+using Repositories.Contracts;
+using TelerikColours.Services;
+
+namespace TelerikColours.Tests.Services.JobServiceTests
+{
+    [TestFixture]
+    public class GetAllJobsFromByTerm_Should
+    {
+        [Test]
+        public void ReturnCorrectResultsAlphabetically_WhenCalled()
+        {
+            // Arrange
+            var stubbedRepository = new Mock<IRepository<Job>>();
+            var jobService = new JobService(stubbedRepository.Object);
+            var searchedTerm = "a";
+
+            var expectedCollection = new List<Job>()
+            {
+                new Job() {JobTitle = "aa", JobDescription = "aa", CompanyName = "aa"},
+                new Job() {JobTitle= "ba", JobDescription = "ab", CompanyName = "ab" },
+                new Job() {JobTitle = "ca", JobDescription = "ac", CompanyName = "a" }
+            }.AsQueryable();
+
+            stubbedRepository.Setup(s => s.All).Returns(expectedCollection);
+
+            // Act
+            var actualCollection = jobService.GetAllJobsFromByTerm(searchedTerm);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedCollection, actualCollection);
+        }
+    }
+}
