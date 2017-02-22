44cc912908d837200febbd2459584d606c7f948e
BoykoBonev <boykobonev@outlook.com>
2017-02-22 11:23:45 +0200

    Added tests for GetDetailedFlight method on flight service

---
+++ TelerikColours.Tests/Services/FlightServiceTests/GetDetailedFlight_Should.cs
@@ -0,0 +1,39 @@
+﻿using Models;
+using Moq;
+using NUnit.Framework;
+using Repositories.Contracts;
+using TelerikColours.Services;
+using TelerikColours.Services.Contracts.Factories;
+
+namespace TelerikColours.Tests.Services.FlightServiceTests
+{
+    [TestFixture]
+    public class GetDetailedFlight_Should
+    {
+        [TestCase(23)]
+        [TestCase(6)]
+        public void ReturnExpectedFlightFromFlightRepositoryGetById_WhenIsCalledWithExpectedId(int id)
+        {
+            // Arrange
+            var flightRepositoryStub = new Mock<IRepository<Flight>>();
+            var airportRepositoryStub = new Mock<IRepository<Airport>>();
+            var cityRepositoryStub = new Mock<IRepository<City>>();
+            var mappedFactory = new Mock<IMappedClassFactory>();
+            var unitOfWorkStub = new Mock<IUnitOfWork>();
+            var flightService = new FlightService(airportRepositoryStub.Object, unitOfWorkStub.Object, flightRepositoryStub.Object, cityRepositoryStub.Object, mappedFactory.Object);
+
+            var flight = new Flight()
+            {
+                Id = id
+            };
+
+            flightRepositoryStub.Setup(f => f.GetById(id)).Returns(flight);
+
+            // Act
+            var actualFlight = flightService.GetDetailedFlight(id);
+
+            // Assert
+            Assert.AreEqual(flight, actualFlight);
+        }
+    }
+}
